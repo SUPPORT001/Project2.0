f00c95e289f5d0e4718e0035c829c27f95064c28
Paxila <paxila.1.130@gmail.com>
2022-05-25 15:06:20 +0700

    gegegege

--- Assets/Scripts/testScr.cs
+++ Assets/Scripts/testScr.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class testScr : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
-        
+    
+    Debug.Log("swove sisi")
     }
 
     // Update is called once per frame
