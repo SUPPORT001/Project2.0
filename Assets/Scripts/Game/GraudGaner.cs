d3465efd103fd767d8f5964796649ac66a061632
Paxila <paxila.1.130@gmail.com>
2022-06-01 13:53:45 +0700

    555

--- Assets/Scripts/Game/GraudGaner.cs
+++ Assets/Scripts/Game/GraudGaner.cs
@@ -8,6 +8,7 @@ public class GraudGaner : MonoBehaviour
     private Tilemap tilemapRo;
     public List<TileBase> tilesGr = new List<TileBase>();
     public List<TileBase> tilesRo = new List<TileBase>();
+    public List<TileBase> tilesEr = new List<TileBase>();
     public Vector2Int map;
     public List<Vector3Int> roadVec = new List<Vector3Int>();
 
@@ -35,7 +36,7 @@ public class GraudGaner : MonoBehaviour
     void RoadGener()
     {
 
-        Vector3Int randPos( Vector3Int lastVec) //1 меняемый вектор 2 прошлый вектор
+        /*Vector3Int randPos( Vector3Int lastVec, List<Vector3int> listVec) //прошлый вектор
         {
             Vector3Int vec = lastVec;
             switch (UnityEngine.Random.Range(1, 4))
@@ -43,7 +44,7 @@ public class GraudGaner : MonoBehaviour
                 case 1:
                     if (lastVec == new Vector3Int(vec.x - 1, vec.y, vec.z)) goto case 4;
                     else vec.x--;
-                    
+
                     break;
                 case 2:
                     if (lastVec == new Vector3Int(vec.x + 1, vec.y, vec.z)) goto case 5;
@@ -53,7 +54,7 @@ public class GraudGaner : MonoBehaviour
                     vec.y++;
                     break;
                 case 4:
-                    if(UnityEngine.Random.Range(0,2) == 0) vec.x++;
+                    if (UnityEngine.Random.Range(0, 2) == 0) vec.x++;
                     else vec.y++;
                     break;
                 case 5:
@@ -63,6 +64,33 @@ public class GraudGaner : MonoBehaviour
             }
             
             return vec;
+        }*/
+
+        Vector3Int randPos(Vector3Int lastVec, int count) //прошлый вектор
+        {
+            Vector3Int vec = lastVec;
+
+            if (count % 2 == 0)
+            {
+                vec.y++;
+            }
+            else if (UnityEngine.Random.Range(0, 2) == 0)
+            {
+                vec.x++;
+            }
+            else vec.x--;
+
+            return vec;
+        }
+
+        Vector3Int mirror(Vector3Int a, Vector3Int n)// a - отражаемый вектор, n - нормаль в точке пересечения
+        {
+            Vector3Int b = Vector3Int.zero;
+            int k = n.x * a.x + n.y * a.y + n.z * a.z;
+            b.x = a.x + n.x * k * -2;
+            b.y = a.y + n.y * k * -2;
+            b.z = a.z + n.z * k * -2;
+            return b;
         }
         Vector3Int setTileVec = new Vector3Int(map.x / 2, 0, -1);
         tilemapRo.SetTile(setTileVec, tilesRo[5]); // начало пути
@@ -72,19 +100,22 @@ public class GraudGaner : MonoBehaviour
         while (roadVec[countTiles].y < map.y / 2)
         {
             countTiles++;
-            roadVec.Add(randPos(roadVec[countTiles - 1])) ;
-            
-            Debug.Log(roadVec[countTiles] + " " + countTiles);
-            //tilemapRo.SetTile(randPos(setTileVec, roadVec[countTiles - 1]), tilesRo[Random.Range(0,tilesGr.Count)]);
-            //setTileVec[countTiles]= 
-            if (countTiles > 10000) return;
+            roadVec.Add(randPos(roadVec[countTiles - 1], countTiles));
+
         };
 
-        for(int i = 0; i < roadVec.Count; i++)
+
+        /* while (roadVec[countTiles].y < map.y)
+         {
+             roadVec.Add(mirror(roadVec[invertTiles], new Vector3Int(map.x, map.y, 0)));
+             if (countTiles > 100) return;
+         }*/
+
+        for (int i = 0; i < roadVec.Count; i++)
         {
-            tilemapRo.SetTile(roadVec[i], tilesRo[UnityEngine.Random.Range(0,8)]);
+            tilemapRo.SetTile(roadVec[i], tilesRo[UnityEngine.Random.Range(0, 8)]);
         }
-      
+
     }
     void Update()
     {
