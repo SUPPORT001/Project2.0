98fdb8c260a2f38f5a8360bc72919468fd3b099a
SUPPORT001 <78125365+SUPPORT001@users.noreply.github.com>
2022-06-29 10:18:45 +0700

    commit29.06

--- Assets/Scripts/Menu/Login.cs
+++ Assets/Scripts/Menu/Login.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
 
 public class Login : MonoBehaviour
 {
     public InputField login_text;
     public InputField password_text;
 
-    private bool Check()
+    public List<Users> users = new List<Users>();
+
+    [SerializeField] private string path_getUsers;
+
+    public bool Check()
     {
         string log = login_text.GetComponent<InputField>().text;
         string pas = password_text.GetComponent<InputField>().text;
@@ -21,45 +26,67 @@ public class Login : MonoBehaviour
         }
         if(pas.Length == 0)
         {
-            Debug.Log("Поле <Пароль> не должно быть пустым.");
+            Debug.LogError("Поле <Пароль> не должно быть пустым.");
             return false;
         }
         return true;
     }
 
-    public void Click_authorization()
+    public void getUsers_click()
     {
-        if(Check())
+        StartCoroutine(getUsers());
+    }
+
+    IEnumerator getUsers()
+    {
+        //WWWForm form = new WWWForm();
+        using (UnityWebRequest www = UnityWebRequest.Get(path_getUsers))
         {
+            yield return www.SendWebRequest();
+            if (UnityWebRequest.Result.ConnectionError == UnityWebRequest.Result.ProtocolError)
+            {
+                
+            }
+            else
+            {
+                Users unit = JsonUtility.FromJson<Users>(www.downloadHandler.text);
+                Debug.Log(JsonUtility.FromJson<Users>(www.downloadHandler.text));
+                //Debug.Log("id: " unit.id);
+            }
             
         }
+        //yield return form;
     }
+
+    
 
     public void Click_registration()
     {
-        string log = login_text.GetComponent<InputField>().text;
-        string pas = password_text.GetComponent<InputField>().text;
 
-        if(log.Length == 0)
-        {
-            Debug.LogError("Поле <Логин> не должно быть пустым!");
-            return;
-        }
-        if(pas.Length == 0)
-        {
-            Debug.Log("Поле <Пароль> не должно быть пустым.");
-            return;
-        }
-
-        if(PlayerPrefs.HasKey(log))
-        {
-            Debug.LogWarning("Пользователь с таким логином уже есть!");
-        }
-        else
-        {
-            PlayerPrefs.SetString(log, pas);
-            PlayerPrefs.Save();
-            Debug.Log("Авторизация успешна.");
-        }
     }
+
+    
+
+    /*
+    IEnumerator Img()
+    {
+        //Texture2D texture = profileImage.canvasRenderer.GetMaterial().mainTexture as Texture2D;
+        var request = UnityWebRequestTexture.GetTexture("https://pp.userapi.com/c841023/v841023649/761a/0TLleJA7dSk.jpg");
+        yield return request.SendWebRequest();
+        Texture2D response = (DownloadHandlerTexture.GetContent(request));
+        print(request);
+        GameObject go = new GameObject("image");
+        Sprite sp = Sprite.Create(response, new Rect(0, 0, response.width, response.height), Vector2.zero);
+        go.AddComponent<Image>().sprite = sp;
+        go.Equals(enabled);
+        request.Dispose();
+    }
+    */
+}
+
+public class Users
+{
+    public int id;
+    public string name;
+    public string admin;
 }
