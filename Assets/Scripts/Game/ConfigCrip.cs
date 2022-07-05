57bb706ab006d7ea3c8671941a502bc47b078d6c
SUPPORT001 <78125365+SUPPORT001@users.noreply.github.com>
2022-07-06 05:19:29 +0700

    second3399

---
+++ Assets/Scripts/Game/ConfigCrip.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ConfigCrip : object
+{
+    public static string knight_name = "Knight";
+    public static float knight_HP = 150;
+    public static float knight_speed = 8;
+    public static float knight_ATK = 20;
+    public static float knight_speedATK = 20;
+    public static float knight_distant = 2;
+    public static string knight_typeAttack = "Melee";
+    public static int knight_moneyCost = 25;
+
+    public static string archer_name = "Archer";
+    public static float archer_HP = 75;
+    public static float archer_speed = 10;
+    public static float archer_ATK = 5;
+    public static float archer_speedATK = 20;
+    public static float archer_distant = 5;
+    public static string archer_typeAttack = "Archer";
+    public static int archer_moneyCost = 35;
+
+    public static string rex_name = "Rex";
+    public static float rex_HP = 75;
+    public static float rex_speed = 13;
+    public static float rex_ATK = 5;
+    public static float rex_speedATK = 10;
+    public static float rex_distant = 2;
+    public static string rex_typeAttack = "Melee";
+    public static int rex_moneyCost = 35;
+
+    public static string wizard_name = "Wizard";
+    public static float wizard_HP = 80;
+    public static float wizard_speed = 8;
+    public static float wizard_ATK = 6;
+    public static float wizard_speedATK = 20;
+    public static float wizard_distant = 5;
+    public static string wizard_typeAttack = "Magik";
+    public static int wizard_moneyCost = 50;
+
+    public static string tank_name = "Tank";
+    public static float tank_HP = 500;
+    public static float tank_speed = 6;
+    public static float tank_ATK = 50;
+    public static float tank_speedATK = 20;
+    public static float tank_distant = 2;
+    public static string tank_typeAttack = "Melee";
+    public static int tank_moneyCost = 100;
+}
