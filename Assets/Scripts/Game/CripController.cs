c8b9ca5e05967b3722ce7f7ec8582c7d32dffac9
SUPPORT001 <78125365+SUPPORT001@users.noreply.github.com>
2022-07-06 05:13:03 +0700

    second

---
+++ Assets/Scripts/Game/CripController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class CripController : MonoBehaviourPunCallbacks, IPunObservable
+{
+    private PhotonView photonView;
+    private SpriteRenderer spriteRenderer;
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (!photonView.IsMine) return;
+
+        if (Config.indexPlayer == 0)
+        {
+            transform.position += Vector3.right * 3f * Time.deltaTime;
+        }
+        else
+        {
+            transform.position += Vector3.left * 3f * Time.deltaTime;
+        }
+    }
+}
