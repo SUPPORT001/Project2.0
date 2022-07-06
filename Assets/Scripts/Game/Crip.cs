5e7f6b2421264dbd4fd6f433d9a12e5a3a41e354
Paxila <paxila.1.130@gmail.com>
2022-07-06 23:44:29 +0700

    Комменты

--- Assets/Scripts/Game/Crip.cs
+++ Assets/Scripts/Game/Crip.cs
@@ -3,9 +3,6 @@ using UnityEngine;
 
 public class Crip : MonoBehaviour
 {
-    //List<Vector3> cripPath = new List<Vector3>();
-    //List<Vector3> cripPathReverse = new List<Vector3>();
-
     public string name;
     public float HP;
     public float speed;
@@ -15,7 +12,8 @@ public class Crip : MonoBehaviour
     public string typeAttack;
     public int moneyCost;
 
-    public Crip(string name, float hP, float speed, float aTK, float speedATK, float distant, string typeAttack, int moneyCost)
+    //Функция для передачи переменных в другие скрипты
+    public Crip(string name, float hP, float speed, float aTK, float speedATK, float distant, string typeAttack, int moneyCost) 
     {
         this.name = name;
         this.HP = hP;
@@ -26,17 +24,7 @@ public class Crip : MonoBehaviour
         this.typeAttack = typeAttack;
         this.moneyCost = moneyCost;
     }
-    //public Crip(Crip crip)
-    //{
-    //    this.name = crip.name;
-    //    this.HP = crip.HP;
-    //    this.speed = crip.speed;
-    //    this.ATK = crip.ATK;
-    //    this.speedATK = crip.speedATK;
-    //    this.distant = crip.distant;
-    //    this.typeAttack = crip.typeAttack;
-    //    this.moneyCost = crip.moneyCost;
-    //}
+ 
     public void Gett(Crip crip)
     {
         this.name = crip.name;
@@ -47,70 +35,5 @@ public class Crip : MonoBehaviour
         this.distant = crip.distant;
         this.typeAttack = crip.typeAttack;
         this.moneyCost = crip.moneyCost;
-    }
-    private void Start()
-    {
-        //switch (transform.name )
-        //{
-        //    case "knight":
-        //        CripStat.Knight knight = new CripStat.Knight();
-        //        name =  knight.name;
-        //        HP = knight.HP;
-        //        speed = knight.speed;
-        //        ATK = knight.ATK;
-        //        speedATK = knight.speedATK;
-        //        distant = knight.distant;
-        //        typeAttack = knight.typeAttack;
-        //        moneyCost = knight.moneyCost;
-        //        break;
-        //    case "archer":
-        //        CripStat.Archer archer = new CripStat.Archer();
-        //        name = archer.name;
-        //        HP = archer.HP;
-        //        speed = archer.speed;
-        //        ATK = archer.ATK;
-        //        speedATK = archer.speedATK;
-        //        distant = archer.distant;
-        //        typeAttack = archer.typeAttack;
-        //        moneyCost = archer.moneyCost;
-        //        break;
-        //    case "wizard":
-        //        CripStat.Wizard wizard = new CripStat.Wizard();
-        //        name = wizard.name;
-        //        HP = wizard.HP;
-        //        speed = wizard.speed;
-        //        ATK = wizard.ATK;
-        //        speedATK = wizard.speedATK;
-        //        distant = wizard.distant;
-        //        typeAttack = wizard.typeAttack;
-        //        moneyCost = wizard.moneyCost;
-        //        break;
-
-        //    case "rex":
-        //        CripStat.Rex rex = new CripStat.Rex();
-        //        name = rex.name;
-        //        HP = rex.HP;
-        //        speed = rex.speed;
-        //        ATK = rex.ATK;
-        //        speedATK = rex.speedATK;
-        //        distant = rex.distant;
-        //        typeAttack = rex.typeAttack;
-        //        moneyCost = rex.moneyCost;
-
-        //        break;
-
-        //    case "tank":
-        //        CripStat.Tank tank = new CripStat.Tank();
-        //        name = tank.name;
-        //        HP = tank.HP;
-        //        speed = tank.speed;
-        //        ATK = tank.ATK;
-        //        speedATK = tank.speedATK;
-        //        distant = tank.distant;
-        //        typeAttack = tank.typeAttack;
-        //        moneyCost = tank.moneyCost;
-        //        break;
-        //}
-       
     }
 }
